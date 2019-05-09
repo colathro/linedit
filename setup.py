50873515dbce880ff33758759fd3066db7e63701
Colton Lathrop <colathro@AENIMA7.northamerica.corp.microsoft.com>
2019-05-08 21:22:01 -0500

    added medit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='linedit',
-      version='1.0',
+      version='1.1',
       description='Simple Interface to Edit specific lines in files.',
       url='https://github.com/colathro/linedit',
       download_url='https://github.com/colathro/linedit/archive/1.0.tar.gz',
